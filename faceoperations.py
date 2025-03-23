3f7e11bf655ccc31381a149db04afe0861483de5
Aditya Mitra <adityaarghya0@gmail.com>
2025-03-23 09:32:24 +0530

    Update faceoperations.py

--- faceoperations.py
+++ faceoperations.py
@@ -4,7 +4,7 @@ import base64
 def facial_recognition(imga1, imga2):
     print("Image 1: ", imga1)
     print("Image 2: ", imga2)
-    return 100 #Modify it to have face recog
+    return True #Modify it to have face recog
 
     
 #It is recommended to develop the pipelines in a seperate file

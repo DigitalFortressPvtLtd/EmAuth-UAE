d51d43b698e4dcb141c7c79eee926ee8e78d65d5
Aditya Mitra <adityaarghya0@gmail.com>
2025-03-23 08:54:50 +0530

    Update masterurl.py

--- masterurl.py
+++ masterurl.py
@@ -2,7 +2,7 @@ deployed_domain='mauthn.mukham.in'
 
 import requests
 def check_domain_valid():
-	resp=requests.get('https://{deployed_domain}/checkdomain', verify=False)
+	resp=requests.get(f'https://{deployed_domain}/checkdomain', verify=False)
 	data=resp.text
 	tempf=open('/etc/mauthn/temp','r')
 	data2=tempf.read()

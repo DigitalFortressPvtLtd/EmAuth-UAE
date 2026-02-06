2f27da403f03f88a104ea52b72b6359719c43a3f
Aditya Mitra <adityaarghya0@gmail.com>
2026-02-06 09:57:00 +0300

    Email

--- emailops.py
+++ emailops.py
@@ -9,7 +9,7 @@ def sendSignupEmail(email,token):
 	sendmail(email, message)
 
 def sendNewReqMail(email, requester):
-	message = "Subject:MAuthN Authentication Request\n\nNew authentication request on MAuthN. Details:\n"+requester+"\n\nCheck MAuthN app for more details."
+	message = "Subject:MAuthN Authentication Request\n\nNew authentication request on MAuthN. Details:\n"+requester+"\n\nCheck the app for more details."
 	sendmail(email, message)
 
 def sendSuccessReqMail(email, requester):
@@ -21,23 +21,23 @@ def adminPromoteMail(email):
 	sendmail(email, message)
 
 def signInitMail(email, title):
-	message = f'Subject:MAuthN New Document signing request\n\nYou have a request to sign a document for {title}. Check MAuthN app for more details.'
+	message = f'Subject:MAuthN New Document signing request\n\nYou have a request to sign a document for {title}. Check the app for more details.'
 	sendmail(email, message)
 
 def signReminderMail(email, title):
-	message = f'Subject:MAuthN Reminder Document signing \n\nYou have a pending request to sign a document for {title}. Check MAuthN app for more details.'
+	message = f'Subject:MAuthN Reminder Document signing \n\nYou have a pending request to sign a document for {title}. Check the app for more details.'
 	sendmail(email, message)
 
 def signCompleteMail(email, title, signer):
-	message = f'Subject:MAuthN Document signing complete\n\nDocument signing for {title} is completed by {signer}. Check MAuthN app for more details.'
+	message = f'Subject:MAuthN Document signing complete\n\nDocument signing for {title} is completed by {signer}. Check the app for more details.'
 	sendmail(email, message)
 
 def appUpdate(emaillist):
-	message = "Subject:MAuthN App Update Available\n\nA new update for MAuthN app is available. Download it for your phone from mauthn.mukham.in"
+	message = "Subject:MAuthN App Update Available\n\nA new update for MAuthN app is available. Download it for your phone from your administrator."
 	for email in emaillist:
 		sendmail(email, message)
 
 def featureDropMail(emaillist, name, txt):
-	message = f'Subject:MAuthN Feature Drop {name}\n\nNew feature {name} available. {txt}'
+	message = f'Subject:Feature Drop {name}\n\nNew feature {name} available. {txt}'
 	for email in emaillist:
 		sendmail(email, message)

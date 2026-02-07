025f03372744b3ab6c0964097d683349e76fa9d2
Aditya Mitra <adityaarghya0@gmail.com>
2026-02-07 09:08:10 +0300

    Update masterurl.py

--- masterurl.py
+++ masterurl.py
@@ -1,4 +1,4 @@
-deployed_domain='onprem.mukham.in'
+deployed_domain='emauth.indominuslabs.in'
 
 import requests
 def check_domain_valid():
@@ -7,3 +7,4 @@ def check_domain_valid():
 	tempf=open('/etc/mauthn/temp','r')
 	data2=tempf.read()
 	return data==data2
+

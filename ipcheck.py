f75b3bee7476aa578edc8e8bedacba486359e562
Aditya Mitra <adityaarghya0@gmail.com>
2026-02-08 11:22:19 +0300

    Update ipcheck.py

--- ipcheck.py
+++ ipcheck.py
@@ -1,7 +1,7 @@
 import requests, json
 import re
 
-def getStatus(ip_address):
+def getStatus(ip_address, product=''):
   ipv4_pattern = r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'
   match = re.search(ipv4_pattern, ip_address)
   ip_address=match.group()

d11a8ed461e9b817d69312197d7e26473d5d4b68
Aditya Mitra <adityaarghya0@gmail.com>
2026-02-12 17:28:07 +0300

    Update awsco.py

--- awsco.py
+++ awsco.py
@@ -1,7 +1,7 @@
 # AWS Credentials & Configuration
 AWS_ACCESS_KEY = "AKIAVNGQ5QCU45F5FLO4"
 AWS_SECRET_KEY = "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"
-AWS_REGION = "me-central-1"
+AWS_REGION = "us-east-1" #"me-central-1"
 SERVICE = "rekognition"
 HOST = f"rekognition.{AWS_REGION}.amazonaws.com"
 ENDPOINT = f"https://{HOST}"

cb84324a4e7ee23cb17344847fd4cbd564506ec3
Aditya Mitra <adityaarghya0@gmail.com>
2026-02-12 17:35:10 +0300

    Update faceoperations.py

--- faceoperations.py
+++ faceoperations.py
@@ -131,4 +131,5 @@ def facial_recognition_aws(base64_img1, base64_img2):
 
 def facial_recognition(base64_img1, base64_img2):
     return True #Comment this line out when you fix the AWS
-    #facial_recognition(base64_img1, base64_img2) #Uncomment this line after fixing the AWS
+    #facial_recognition_aws(base64_img1, base64_img2) #Uncomment this line after fixing the AWS
+
